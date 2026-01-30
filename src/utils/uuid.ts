1844dea70a94042c2f795e827917eec03619e33c
Dennis Murphy <dennis_j_murphy@mac.com>
2026-01-30 20:44:10 +0100

    swapped out uuid for expo-crypto

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,6 +1,6 @@
 // src/utils/uuid.ts
-import { v4 as uuidv4 } from 'uuid';
+import * as Crypto from 'expo-crypto';
 
 export function generateId(): string {
-  return uuidv4();
+  return Crypto.randomUUID();
 }
