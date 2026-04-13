6b1a4cb7fb9ad69fde636e7f98f04359c2e15d48
Dennis Murphy <dennis_j_murphy@mac.com>
2026-04-13 14:40:12 +0200

    testing cleanup

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
   moduleNameMapper: {
     '^expo-sqlite$': '<rootDir>/src/__mocks__/expo-sqlite.ts',
     '^expo-crypto$': '<rootDir>/src/__mocks__/expo-crypto.ts',
-    '^expo-notifications$': '<rootDir>/src/__mocks__/expo-notifications.ts',
     '^expo-sharing$': '<rootDir>/src/__mocks__/expo-sharing.ts',
     '^expo-file-system$': '<rootDir>/src/__mocks__/expo-file-system.ts',
     '^react-native$': '<rootDir>/src/__mocks__/react-native.ts',
